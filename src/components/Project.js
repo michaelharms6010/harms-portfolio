6be6dade26c36137392993062632364bcfd527d7
Michael Harms <michael.harms6010@gmail.com>
2020-04-01 10:26:01 -0500

    adds alt tag

--- src/components/Project.js
+++ src/components/Project.js
@@ -4,8 +4,8 @@ export default function Project({image, description, url}) {
 
     return(
         <div className="project">
-            <a href={url} target="_blank">
-                <img className="project-image" src={image} />
+            <a rel="noopener noreferrer" href={url} target="_blank">
+                <img alt={description.split(" ")[0]}className="project-image" src={image} />
             </a>
             <p classname="project-description">{description}</p>
         </div>
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -10,17 +10,17 @@ import ZecpagesFront from "../assets/ZecpagesFront.PNG";
 import Project from "./Project";
 
 let projects = [
-    {image: citrics, description: "Citrics Description", url: "https://v1.citrics.io"},
-    {image: zecmailer, description: "Zecmailer Description", url: "https://zecmailer.com"},
-    {image: ZecpagesFront, description: "Zecpages Frontpage", url: "https://zecpages.com"},
-    {image: ZecpagesBoard, description: "Zecpages Board", url: "https://zecpages.com/board"}]
+    {image: citrics, description: "Citrics Description", url: "https://v1.citrics.io", github: "https://github.com/michaelharms6010/city-data-comparison-fe"},
+    {image: zecmailer, description: "Zecmailer Description", url: "https://zecmailer.com", github: "https://github.com/michaelharms6010/zecmailer"},
+    {image: ZecpagesFront, description: "Zecpages Frontpage", url: "https://zecpages.com", github: "https://github.com/michaelharms6010/fe-zeitpages"},
+    {image: ZecpagesBoard, description: "Zecpages Board", url: "https://zecpages.com/board", github: "https://github.com/michaelharms6010/fe-zeitpages"}]
 
 export default function Projects() {
 
 
     return (
         <>
-        <h2>Here are some products I've built:</h2> 
+        <h2>Some open-source products I've built:</h2> 
         <div className="project-grid">
             {projects.map(project => <Project image={project.image} description={project.description} url={project.url} />)}
 
