a94729624fe3e2298553a1684280dc66a4f42dd8
Michael Harms <michael.harms6010@gmail.com>
2020-04-01 09:34:30 -0500

    adds sass

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,8 +1,13 @@
 import React from "react";
+import {Link} from "react-router-dom"
 
 export default function Navigation() {
 
     return (
-        <h1>Hi from Navigation.js</h1>
+        <div className="main-nav">
+            <Link to="/bio">Bio</Link>
+            <Link to="/projects">Projects</Link>
+            <Link to="/contact">Contact</Link>
+        </div>
     )
 }
