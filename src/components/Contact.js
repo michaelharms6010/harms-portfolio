150cb703da7cd5f6755e0bed9e2e5bde92e760e3
Michael Harms <michael.harms6010@gmail.com>
2020-04-01 11:04:29 -0500

    adds icons

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,14 +1,18 @@
 import React, {useState} from "react";
 import './Contact.scss';
 
+import github from "../assets/githublogo.png"
+import twitter from "../assets/twitterlogo.png"
+import zcash from "../assets/zcashlogo.png"
+import gmail from "../assets/gmaillogo.png"
 
 
-const contact = {
-    twitter: "https://twitter.com/michaelharms70",
-    github: "https://github.com/michaelharms6010",
-    email: "mailto:michael.harms6010@gmail.com",
-    zcash: "zs1zxeehvq02nf0javeygdxnj6a78quvvlu7gsgg0e39n4uvp9hpdnyy3l4e494vt5kp4wjgrm7mtr"
-}
+const contact = [
+    {url: "https://twitter.com/michaelharms70", icon: twitter},
+    {url: "https://github.com/michaelharms6010", icon: github},
+    {url: "mailto:michael.harms6010@gmail.com", icon: gmail},
+    {url: "zs1zxeehvq02nf0javeygdxnj6a78quvvlu7gsgg0e39n4uvp9hpdnyy3l4e494vt5kp4wjgrm7mtr", icon: zcash},
+]
 
 export default function Contact() {
     const [authority, setAuthority] = useState("I love authority!")
@@ -24,9 +28,12 @@ export default function Contact() {
             <h2>Contact me</h2>
             <p>I'm a team player and extremely conformist! <span onMouseEnter={handleEnter} onMouseLeave={handleLeave}>{authority}</span></p>
             <div className="contact-icons">
-                {Object.keys(contact).map(item => 
-                    <a href={contact[item]}>{item}</a>   
-                )}
+                {contact.map(item => 
+                <a href={item.url}>
+                    <img src={item.icon} />
+                </a>)
+
+                }
             </div>
         
         </div>
