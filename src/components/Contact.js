7777e908e7b0da7fd661f08cae437411360c03ab
Michael Harms <michael.harms6010@gmail.com>
2020-04-01 10:34:59 -0500

    maps contact info

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,9 +1,31 @@
-import React from "react";
+import React, {useState} from "react";
 import './Contact.scss';
 
-export default function Contact() {
+const contact = {
+    twitter: "https://twitter.com/michaelharms70",
+    github: "https://github.com/michaelharms6010",
+    email: "michael.harms6010@gmail.com"
+}
 
+export default function Contact() {
+    const [authority, setAuthority] = useState("I love authority!")
+
+    const handleEnter = e => {
+        setAuthority("fuck you")
+    }
+    const handleLeave = e => {
+        setAuthority("I love authority!")
+    }
     return (
-        <h1>Hi from Contact.js</h1>
+        <div className="contact-page">
+            <h2>Contact me</h2>
+            <p>I'm a team player and extremely conformist! <span onMouseEnter={handleEnter} onMouseLeave={handleLeave}>{authority}</span></p>
+            <div className="contact-icons">
+                {Object.keys(contact).map(item => 
+                    <a href={contact[item]}>{item}</a>   
+                )}
+            </div>
+        
+        </div>
     )
 }
