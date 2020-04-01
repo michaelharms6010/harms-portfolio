456695f5093ef5a763996b94bbcc38d92d4deb81
Michael Harms <michael.harms6010@gmail.com>
2020-04-01 09:58:23 -0500

    writes basic bio

--- src/components/Bio.js
+++ src/components/Bio.js
@@ -4,6 +4,11 @@ import './Bio.scss';
 export default function Bio() {
 
     return (
-        <h1>Good day!</h1>
+        <div className="bio">
+            <h1>Good day!</h1>
+            <p>Hi! I'm Michael. I'm a full stack developer building open source educational, entertainment, and cryptocurrency products.</p>
+            <h2>Team Leadership</h2>
+            <p>As a Team Lead in Lambda School's WEB program, I mentor new web developers through their first taste of HTML/CSS/Javascript, all the way through their first React and Node apps. Along the way I also am a PM for student builds, basically week-long hackathons where cross-functional teams apply what they've learned to building a real app in an Agile team environment.</p>
+        </div>
     )
 }
