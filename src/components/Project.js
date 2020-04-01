7036922698971f652f426ebdde0125b0833bed5e
Michael Harms <michael.harms6010@gmail.com>
2020-04-01 10:17:42 -0500

    maps data

--- src/components/Project.js
+++ src/components/Project.js
@@ -2,5 +2,10 @@ import React from "react";
 
 export default function Project({image, description}) {
 
-
+    return(
+        <div className="project">
+            <img className="project-image" src={image} />
+            <p classname="project-description">{description}</p>
+        </div>
+    )
 }
