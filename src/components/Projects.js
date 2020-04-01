07bae57935f619960b5ae9d995a51b3ab58f4a4d
Michael Harms <michael.harms6010@gmail.com>
2020-04-01 10:12:19 -0500

    imports images

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,9 +1,17 @@
 import React from "react";
 import './Projects.scss';
+import Project from "./Project";
+import citrics from "../assets/citrics.PNG";
+import zecmailer from "../assets/zecmailer.PNG";
+import ZecpagesBoard from "../assets/ZecpagesBoard.PNG";
+import ZecpagesFront from "../assets/ZecpagesFront.PNG";
 
 export default function Projects() {
 
     return (
-        <h1>Hi from Projects.js</h1>
+        <div className="project-grid">
+            
+
+        </div>
     )
 }
