9fe7caebcd1eb9962288ce7aed8e16f79b247310
Michael Harms <michael.harms6010@gmail.com>
2020-04-01 09:27:05 -0500

    adds routing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import './App.css';
+import {BrowserRouter as Router, Route} from "react-router-dom";
+import Navigation from "./components/Navigation";
 
 function App() {
   return (
-    <div className="App">
-
-    </div>
+    <Router>
+      <div className="App">
+        <Navigation />
+      </div>
+    </Router>
   );
 }
 
