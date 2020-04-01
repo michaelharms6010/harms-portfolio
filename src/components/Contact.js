2a8b42925b56fda55617f428bddca9300eef4f49
Michael Harms <michael.harms6010@gmail.com>
2020-04-01 10:52:43 -0500

    adds zcash

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -4,7 +4,8 @@ import './Contact.scss';
 const contact = {
     twitter: "https://twitter.com/michaelharms70",
     github: "https://github.com/michaelharms6010",
-    email: "michael.harms6010@gmail.com"
+    email: "mailto:michael.harms6010@gmail.com",
+    zcash: "zs1zxeehvq02nf0javeygdxnj6a78quvvlu7gsgg0e39n4uvp9hpdnyy3l4e494vt5kp4wjgrm7mtr"
 }
 
 export default function Contact() {
