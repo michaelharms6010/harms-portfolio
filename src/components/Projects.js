a4a0d8cc4801d15abe1b5f19bfe325bebea25efa
Michael Harms <michael.harms6010@gmail.com>
2020-04-01 10:14:18 -0500

    assembles projects

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -6,7 +6,14 @@ import zecmailer from "../assets/zecmailer.PNG";
 import ZecpagesBoard from "../assets/ZecpagesBoard.PNG";
 import ZecpagesFront from "../assets/ZecpagesFront.PNG";
 
+let projects = [
+    {image: citrics, description: "Citrics Description"},
+    {image: zecmailer, description: "Zecmailer Description"},
+    {image: ZecpagesFront, description: "Zecpages Frontpage"},
+    {image: ZecpagesBoard, description: "Zecpages Board"}]
+
 export default function Projects() {
+
 
     return (
         <div className="project-grid">
