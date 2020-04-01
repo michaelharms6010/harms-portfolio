9b67010b8638318631400baea85596bbf27bbd03
Michael Harms <michael.harms6010@gmail.com>
2020-04-01 09:53:01 -0500

    scaffolds nav

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,14 +1,19 @@
 import React from "react";
 import {Link} from "react-router-dom"
 import './Navigation.scss';
+import logo from "../logo.svg";
 
 export default function Navigation() {
 
     return (
         <div className="main-nav">
-            <Link to="/bio">Bio</Link>
-            <Link to="/projects">Projects</Link>
-            <Link to="/contact">Contact</Link>
+            <img src={logo} width={100} height={100}/>
+
+            <div className="nav-links">
+                <Link to="/bio">Bio</Link>
+                <Link to="/projects">Projects</Link>
+                <Link to="/contact">Contact</Link>
+            </div>
         </div>
     )
 }
