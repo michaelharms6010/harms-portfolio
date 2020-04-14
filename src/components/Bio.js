4d10b538ea0e1dafaecc8b2c4755657e1c3552d2
Michael Harms <michael.harms6010@gmail.com>
2020-04-13 19:50:24 -0500

    adds bolding adn styles

--- src/components/Bio.js
+++ src/components/Bio.js
@@ -7,15 +7,16 @@ export default function Bio() {
         <div data-aos="zoom-in" className="bio">
             <div className="greeting">
                 <h2>A Hearty Hello to all!</h2>
-                <p>Hi! I'm Michael. I'm a full stack developer building open source educational, entertainment, and cryptocurrency products. A relentless optimist, I'm convinced humanity is at the dawn of a new Golden Age. I aim to bring efficient products to market to help be a part of designing and building a better future for all humans.</p>
+                <p>Hi! I'm Michael. I'm a full stack developer building open source educational, entertainment, and cryptocurrency products. A relentless optimist, I'm convinced humanity is at the dawn of a <strong>new Golden Age.</strong> I aim to bring efficient products to market to help be a part of designing and building a better future for all humans.</p>
+                <p><strong>Technologies Mastered: React, Node, Knex, Postgres, Sass, AWS, Git, Passportjs, zcashd</strong></p>
             </div>
             <div className="team-leadership">
                 <h2>Team Leader</h2>
-                <p>As a Team Lead in Lambda School's WEB program, I mentor new web developers through their first taste of HTML/CSS/Javascript, all the way through their first React and Node apps. Along the way I also am a PM for student builds, basically week-long hackathons where cross-functional teams apply what they've learned to building a real app in an Agile team environment.</p>
+                <p>As a <strong>Team Lead in Lambda School's WEB program,</strong> I mentor new web developers through their first taste of HTML/CSS/Javascript, all the way through their first React and Node apps. Along the way I also am a PM for student builds, basically week-long hackathons where cross-functional teams apply what they've learned to building a real app in an Agile team environment.</p>
             </div>
             <div className="team-leadership">
                 <h2>Open Source Builder</h2>
-                <p>I build free software tools in my spare time which aim democratize access to financial privacy. I love Zcash, Monero, and all cryptocurrencies doing work in this area, who are working to make the dignity and consent that comes with privacy available to all, not just the super-powerful.</p>
+                <p>I build free software tools in my spare time which aim democratize access to <strong>financial freedom.</strong> I love <strong>Zcash, Monero,</strong> and all cryptocurrencies doing work in this area, who are working to make the dignity and consent that comes with privacy available to all, not just the super-powerful.</p>
             </div>
             <Link to="/products"><button className="next-button">See some products I've built!</button></Link>
         </div>
--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,5 +1,5 @@
 import React from "react";
-import {Link} from "react-router-dom"
+import {Link, NavLink} from "react-router-dom"
 import './Navigation.scss';
 import logo from "../logo.svg";
 
@@ -10,9 +10,9 @@ export default function Navigation() {
             <Link to="/"><img alt="Mike's smiling face" src={logo} width={100} height={100}/></Link>
 
             <div className="nav-links">
-                <Link to="/bio">Bio</Link>
-                <Link to="/products">Products</Link>
-                <Link to="/contact">Contact</Link>
+                <NavLink activeClassName="active-link" to="/bio">Bio</NavLink>
+                <NavLink activeClassName="active-link" to="/products">Products</NavLink>
+                <NavLink activeClassName="active-link" to="/contact">Contact</NavLink>
             </div>
         </div>
     )
